c7d7158c7b062c43e7b00f0428488831fdb7b1f0
napnyl <nelson.epn@gmail.com>
2022-05-16 20:00:48 -0500

    Main folders commit

---
+++ StPlanning.WorkFlow/ClassTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StPlanning.WorkFlow
+{
+    public class Strategies
+    {
+        public string Type { get; set; }
+        public string Strategy { get; set; }
+    }
+}
---
+++ StPlanning/Program.cs
@@ -0,0 +1,33 @@
+﻿using DevExpress.LookAndFeel;
+using DevExpress.XtraSplashScreen;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StPlanning.Core
+{
+    static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+
+            //Application.EnableVisualStyles();
+            //UserLookAndFeel.Default.SetSkinStyle("VS2010");
+            //DevExpress.Skins.SkinManager.EnableFormSkins();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //DevExpress.XtraSplashScreen.SplashScreenManager.RegisterUserSkins(typeof(DevExpress.UserSkins.BonusSkins).Assembly);
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new frmDiagramTest());
+            Application.Run(new Main());
+        }
+    }
+}
